6cf6fa1afaf422312eafcc3e9607e09b8037c187
abdullahh10 <abdullahh10@outlook.com>
2023-09-19 00:07:26 +0600

    fixed quick sort

--- Sort/QuickSort.java
+++ Sort/QuickSort.java
@@ -12,26 +12,26 @@ public class  QuickSort {
     public static void quickSort(int[] nums, int left, int right){
         if(left<right){
             int partitionedPosition = partition(nums, left, right);
-            if(partitionedPosition != 0) quickSort(nums, left, partitionedPosition-1);
-            if(partitionedPosition != nums.length-1) quickSort(nums, partitionedPosition+1, right);
+            quickSort(nums, left, partitionedPosition-1);
+            quickSort(nums, partitionedPosition+1, right);
         }
     }
 
     public static int partition(int[] nums, int left, int right){
         int pivotIndex = (int) (left+(Math.floor((right-left)/2.0)));
         int pivot = nums[pivotIndex];
-        System.out.println(Arrays.toString(nums) +" "+left+" "+right+" pivot: "+pivot);
+        swap(nums,pivotIndex,right);
 
         int i=left;
         int j=left-1;
-        while(i<=right){
+        while(i<=right-1){
             if(nums[i]<pivot){
                 j++;
                 swap(nums,i,j);
             }
             i++;
         }
-        swap(nums,++j,pivotIndex);
+        swap(nums,++j,right);
         return j;
     }
 
