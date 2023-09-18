3b2e911307b4f1384a7aa71e3d816797ae2b7804
abdullahh10 <abdullahh10@outlook.com>
2023-09-18 23:44:41 +0600

    quick sort update

--- Sort/QuickSort.java
+++ Sort/QuickSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class  QuickSort {
@@ -11,28 +12,26 @@ public class  QuickSort {
     public static void quickSort(int[] nums, int left, int right){
         if(left<right){
             int partitionedPosition = partition(nums, left, right);
-            quickSort(nums, left, partitionedPosition-1);
-            quickSort(nums, partitionedPosition+1, right);
+            if(partitionedPosition != 0) quickSort(nums, left, partitionedPosition-1);
+            if(partitionedPosition != nums.length-1) quickSort(nums, partitionedPosition+1, right);
         }
     }
 
     public static int partition(int[] nums, int left, int right){
         int pivotIndex = (int) (left+(Math.floor((right-left)/2.0)));
         int pivot = nums[pivotIndex];
+        System.out.println(Arrays.toString(nums) +" "+left+" "+right+" pivot: "+pivot);
 
         int i=left;
         int j=left-1;
-        while(i<right){
-            if(nums[i]<=pivot){
+        while(i<=right){
+            if(nums[i]<pivot){
                 j++;
                 swap(nums,i,j);
-                i++;
             }
-            else{
-                i++;
-            }
+            i++;
         }
-        swap(nums,j+1,pivotIndex);
+        swap(nums,++j,pivotIndex);
         return j;
     }
 
